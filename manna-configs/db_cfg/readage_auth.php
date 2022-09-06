e22e3a65f812ecf216ce61406e0a7e3bc57d9f46
Robert Lefebvre <robert.r.lefebvre@gmail.com>
2022-09-06 17:05:18 -0400

    Delete readage_auth.php

--- manna-configs/db_cfg/readage_auth.php
+++
@@ -1,6 +0,0 @@
-<?php
-			      $servername = "localhost";
-			      $username = "orlandor_readage";
-			      $password = "4GLFxQoErjzjfaNCynkeq3X3CWXHHLlb";
-			      $dbname = "orlandor_agents";
-			      ?>
