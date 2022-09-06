46c0ceb7a8e6733670b354dfb2afb472f1a77e5d
Robert Lefebvre <robert.r.lefebvre@gmail.com>
2022-09-06 17:09:10 -0400

    Delete writage_auth.php

--- manna-configs/db_cfg/writage_auth.php
+++
@@ -1,6 +0,0 @@
-<?php
-			      $servername = "localhost";
-			      $username = "orlandor_writage";
-			      $password = "yCnTGbitlGJQkqn9zYj8";
-			      $dbname = "orlandor_agents";
-			      ?>
