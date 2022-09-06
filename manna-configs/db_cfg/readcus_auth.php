3e2d3896a5e89817becfc0cd15a1b31943a45127
Robert Lefebvre <robert.r.lefebvre@gmail.com>
2022-09-06 17:08:14 -0400

    Delete readcus_auth.php

--- manna-configs/db_cfg/readcus_auth.php
+++
@@ -1,6 +0,0 @@
-<?php
-			      $servername = "localhost";
-			      $username = "orlandor_readcus";
-			      $password = "HyVExQZcPnEh34hDziq8F4n3vO8k3Elp";
-			      $dbname = "orlandor_customers";
-			      ?>
