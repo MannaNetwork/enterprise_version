b9dcc9293a5677c3256751bb77a84b2bcd8bf83c
Robert Lefebvre <robert.r.lefebvre@gmail.com>
2022-09-06 17:10:52 -0400

    Add files via upload

---
+++ manna-configs/db_cfg/readage_auth_template.php
@@ -0,0 +1,11 @@
+<?php
+
+//Enter credentials for the agent database for the user with limited read-only privileges
+
+			      $servername = "localhost";
+			      $username = "_readage";//your server may add a prefix to the user name. If so, add it to the front of the name - example foo_readage
+			      $password = "4GLFxQoErjzjfaNCynkeq3X3CWXHHLlb";
+			      $dbname = "_agents"; //your server may add a prefix to the db name. If so, add it to the front of the name - example foo_agents
+			      
+			      //SAVE THIS FILE AS readage_auth.php (i.e. drop _template from the name)
+			      ?>
