c2c41ee666b0e3e7482f354e7fa551e0e0c917e5
Robert Lefebvre <robert.r.lefebvre@gmail.com>
2022-09-06 17:09:25 -0400

    Delete writcus_auth.php

--- manna-configs/db_cfg/writcus_auth.php
+++
@@ -1,6 +0,0 @@
-<?php
-			      $servername = "localhost";
-			      $username = "orlandor_writcus";
-			      $password = "PRMIJmRh3NCnGkFj0BUc";
-			      $dbname = "orlandor_customers";
-			      ?>
